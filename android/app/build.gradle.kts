de9ff2bac22c669712d273b86627a7d19b27af80
quangn2212 <wuangsotech@gmail.com>
2025-04-22 09:22:20 +0700

    update: add lib denoise

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,6 +28,10 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+
+        ndk {
+            abiFilters += listOf("armeabi-v7a", "arm64-v8a")
+        }
     }
 
     buildTypes {
@@ -35,6 +39,12 @@ android {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+
+    sourceSets {
+        getByName("main") {
+            jniLibs.srcDirs("src/main/jniLibs")
         }
     }
 }
--- android/app/src/main/kotlin/com/example/test_lib_denoise/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/test_lib_denoise/MainActivity.kt
@@ -2,4 +2,12 @@ package com.example.test_lib_denoise
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    init {
+        try {
+            System.loadLibrary("audio_recorder")
+        } catch (e: UnsatisfiedLinkError) {
+            Log.e("MainActivity", "Failed to load libaudio_recorder.so", e)
+        }
+    }
+}
